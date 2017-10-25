89afed1d2cf3128e46fd0ff01d482484c4ce97eb
Llewellyn Falco <isidore@setgame.com>
2017-10-25 11:36:36 -0700

    Rename and remove dead comments

--- ChartSmart/ChartSmart/src/org/zuill/talks/codeexcellence/chartsmart/ChartWindow.java
+++ ChartSmart/ChartSmart/src/org/zuill/talks/codeexcellence/chartsmart/ChartWindow.java
@@ -15,9 +15,6 @@ public class ChartWindow extends JPanel {
 	private String title;
 	private int countOrChartNumber;
 
-	/**
-	 * InitializeDrawArea
-	 */
 	private void initializeDrawArea() {
 		this.setPreferredSize(new Dimension(600, 600));
 
@@ -40,9 +37,6 @@ public class ChartWindow extends JPanel {
 		return title;
 	}
 
-	/**
-	 * Shows the chart
-	 */
 	public void showChart(int countOrChartNumber, String displayName, boolean shouldInitialize) {
 		this.countOrChartNumber = countOrChartNumber;
 		this.jjDisplay = displayName;
@@ -52,30 +46,30 @@ public class ChartWindow extends JPanel {
 		}
 	}
 
-	public void paint(Graphics g) {
-		DrawChart(g);
+	public void paint(Graphics graphics) {
+		DrawChart(graphics);
 	}
 
-	private void DrawChart(Graphics g) {
+	private void DrawChart(Graphics graphics) {
 		// Render chart background
 		if (countOrChartNumber == 406) {
 			if (jjDisplay.equals("rpfll")) {
-				g.setColor(Color.RED);
-				g.fillRect(100, 90, getWidth() - 200, 420);
+				graphics.setColor(Color.RED);
+				graphics.fillRect(100, 90, getWidth() - 200, 420);
 			} else {
-				g.setColor(Color.BLACK);
-				g.fillRect(95, 95, 210, 210);
+				graphics.setColor(Color.BLACK);
+				graphics.fillRect(95, 95, 210, 210);
 			}
 		} else {
 			if (jjDisplay.equals("rpfll")) {
-				g.setColor(Color.BLUE);
-				g.fillOval(100, 100, 450, getHeight() - 150);
+				graphics.setColor(Color.BLUE);
+				graphics.fillOval(100, 100, 450, getHeight() - 150);
 			} else {
-				g.setColor(Color.BLUE);
+				graphics.setColor(Color.BLUE);
 				double isq = 405;
 				float padding = 90;
 				int sc = (int) (isq - padding * 2);
-				g.fillOval(100, 100, sc, sc);
+				graphics.fillOval(100, 100, sc, sc);
 			}
 		}
 
@@ -110,43 +104,43 @@ public class ChartWindow extends JPanel {
 				if (data != null) {
 
 					font = new Font("Arial Black", Font.BOLD, 25);
-					g.setColor(Color.CYAN);
+					graphics.setColor(Color.CYAN);
 					int bottomY = 300;
-					g.fillRect(100, bottomY - 100, 40, 100);
-					g.fillRect(140, bottomY - 200, 40, 200);
-					g.fillRect(180, bottomY - 150, 40, 150);
-					g.fillRect(220, bottomY - 125, 40, 125);
-					g.fillRect(260, bottomY - 170, 40, 170);
-					g.setColor(Color.RED);
-					g.setFont(font);
-					g.drawString(data[0], 130, 250);
-					g.drawString(data[1], 130, 270);
+					graphics.fillRect(100, bottomY - 100, 40, 100);
+					graphics.fillRect(140, bottomY - 200, 40, 200);
+					graphics.fillRect(180, bottomY - 150, 40, 150);
+					graphics.fillRect(220, bottomY - 125, 40, 125);
+					graphics.fillRect(260, bottomY - 170, 40, 170);
+					graphics.setColor(Color.RED);
+					graphics.setFont(font);
+					graphics.drawString(data[0], 130, 250);
+					graphics.drawString(data[1], 130, 270);
 				}
 			} else {
 				int bottomY = 500;
-				g.setColor(Color.CYAN);
-				g.fillRect(112, bottomY - 200, 75, 200);
-				g.fillRect(187, bottomY - 400, 75, 400);
-				g.fillRect(262, bottomY - 300, 75, 300);
-				g.fillRect(337, bottomY - 250, 75, 250);
-				g.fillRect(412, bottomY - 340, 75, 340);
+				graphics.setColor(Color.CYAN);
+				graphics.fillRect(112, bottomY - 200, 75, 200);
+				graphics.fillRect(187, bottomY - 400, 75, 400);
+				graphics.fillRect(262, bottomY - 300, 75, 300);
+				graphics.fillRect(337, bottomY - 250, 75, 250);
+				graphics.fillRect(412, bottomY - 340, 75, 340);
 				font = new Font("Arial Black", Font.BOLD, 55);
-				g.setColor(Color.BLACK);
-				g.setFont(font);
-				g.drawString(data[0], 130, 400);
+				graphics.setColor(Color.BLACK);
+				graphics.setFont(font);
+				graphics.drawString(data[0], 130, 400);
 			}
 		} else {
 			if (jjDisplay.equals("rpfll")) {
 				font = new Font("Bookman Old Style", Font.BOLD, 55);
-				g.setColor(Color.WHITE);
-				g.setFont(font);
-				g.drawString(specialData.get(0), 200, 340);
+				graphics.setColor(Color.WHITE);
+				graphics.setFont(font);
+				graphics.drawString(specialData.get(0), 200, 340);
 			} else {
 				font = new Font("Bookman Old Style", Font.BOLD, 30);
-				g.setFont(font);
-				g.setColor(Color.WHITE);
-				g.drawString(data3point14[0], 145, 205);
-				g.drawString(data3point14[1], 170, 235);
+				graphics.setFont(font);
+				graphics.setColor(Color.WHITE);
+				graphics.drawString(data3point14[0], 145, 205);
+				graphics.drawString(data3point14[1], 170, 235);
 			}
 		}
 
