405c170326e67330cb5b9b369e1fda1f7467b9c7
Llewellyn Falco <isidore@setgame.com>
2017-10-25 09:13:54 -0700

    start

---
+++ ChartSmart/ChartSmart/src/org/zuill/talks/codeexcellence/chartsmart/tests/ChartSmartTest.java
@@ -0,0 +1,47 @@
+package org.zuill.talks.codeexcellence.chartsmart.tests;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.reporters.DelayedClipboardReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.zuill.talks.codeexcellence.chartsmart.ChartSmart;
+import org.zuill.talks.codeexcellence.chartsmart.ChartWindow;
+
+import junit.framework.TestCase;
+
+@UseReporter(DelayedClipboardReporter.class)
+public class ChartSmartTest extends TestCase
+{
+  public void testMainWindow() throws Exception
+  {
+    ChartSmart chartSmart = new ChartSmart();
+    Approvals.verify(chartSmart);
+  }
+  public void testBarChartWindow() throws Exception
+  {
+    ChartWindow chartSmart = new ChartWindow();
+    chartSmart.iniDS(406, "rpfll", true);
+    Approvals.verify(chartSmart);
+    assertEquals("Bar Chart - Single Mode", chartSmart.getTitle());
+  }
+  public void testPieChartWindow() throws Exception
+  {
+    ChartWindow chartSmart = new ChartWindow();
+    chartSmart.iniDS(323, "rpfll", true);
+    Approvals.verify(chartSmart);
+    assertEquals("Pie Chart - Single Mode", chartSmart.getTitle());
+  }
+  public void testBarChartSmallWindow() throws Exception
+  {
+    ChartWindow chartSmart = new ChartWindow();
+    chartSmart.iniDS(406, "shareddisplay", true);
+    Approvals.verify(chartSmart);
+    assertEquals("Bar Chart - Compare Mode", chartSmart.getTitle());
+  }
+  public void testPieChartSmallWindow() throws Exception
+  {
+    ChartWindow chartSmart = new ChartWindow();
+    chartSmart.iniDS(323, "shareddisplay", true);
+    Approvals.verify(chartSmart);
+    assertEquals("Pie Chart - Compare Mode", chartSmart.getTitle());
+  }
+}
