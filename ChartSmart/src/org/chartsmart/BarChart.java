784c6d5dbdcb40eb3476010d4cbd68a79d3e3987
Llewellyn Falco <isidore@setgame.com>
2018-05-03 13:33:48 -0500

    Move render to bar chart

--- ChartSmart/src/org/chartsmart/BarChart.java
+++ ChartSmart/src/org/chartsmart/BarChart.java
@@ -1,5 +1,41 @@
 package org.chartsmart;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
 public class BarChart {
+
+	public void renderChart(IndividualDisplay individualDisplay, Graphics graphics, ChartData chartData) {
+		Font font;
+		if (individualDisplay.mode.equals(IndividualDisplay.SHARED_DISPLAY_MODE)) {
+			if (chartData.data != null) {
+				font = new Font("Arial Black", Font.BOLD, 25);
+				graphics.setColor(Color.CYAN);
+				int bottomY = 300;
+				graphics.fillRect(100, bottomY - 100, 40, 100);
+				graphics.fillRect(140, bottomY - 200, 40, 200);
+				graphics.fillRect(180, bottomY - 150, 40, 150);
+				graphics.fillRect(220, bottomY - 125, 40, 125);
+				graphics.fillRect(260, bottomY - 170, 40, 170);
+				graphics.setColor(Color.RED);
+				graphics.setFont(font);
+				graphics.drawString(chartData.data[0], 130, 250);
+				graphics.drawString(chartData.data[1], 130, 270);
+			}
+		} else {
+			int bottomY = 500;
+			graphics.setColor(Color.CYAN);
+			graphics.fillRect(112, bottomY - 200, 75, 200);
+			graphics.fillRect(187, bottomY - 400, 75, 400);
+			graphics.fillRect(262, bottomY - 300, 75, 300);
+			graphics.fillRect(337, bottomY - 250, 75, 250);
+			graphics.fillRect(412, bottomY - 340, 75, 340);
+			font = new Font("Arial Black", Font.BOLD, 55);
+			graphics.setColor(Color.BLACK);
+			graphics.setFont(font);
+			graphics.drawString(chartData.data[0], 130, 400);
+		}
+	}
 
 }
--- ChartSmart/src/org/chartsmart/IndividualDisplay.java
+++ ChartSmart/src/org/chartsmart/IndividualDisplay.java
@@ -9,10 +9,10 @@ import java.util.List;
 import javax.swing.JPanel;
 
 public class IndividualDisplay extends JPanel {
-	private static final String SHARED_DISPLAY_MODE = "shareddisplay";
+	static final String SHARED_DISPLAY_MODE = "shareddisplay";
 	private static final String SINGLE_MODE = "rpfll";
 	private static final int BAR_CHART = 406;
-	private String mode;
+	String mode;
 	private String title;
 	private int chartType;
 
@@ -69,7 +69,7 @@ public class IndividualDisplay extends JPanel {
 	public void renderChart(Graphics graphics, ChartData chartData) {
 		if (chartType == BAR_CHART) {
 			BarChart barChart = new BarChart();
-			renderBarChart(graphics, chartData);
+			barChart.renderChart(this, graphics, chartData);
 		} else {
 			renderPieChart(graphics, chartData);
 		}
@@ -88,38 +88,6 @@ public class IndividualDisplay extends JPanel {
 			graphics.setColor(Color.WHITE);
 			graphics.drawString(chartData.data3point14[0], 145, 205);
 			graphics.drawString(chartData.data3point14[1], 170, 235);
-		}
-	}
-
-	public void renderBarChart(Graphics graphics, ChartData chartData) {
-		Font font;
-		if (mode.equals(SHARED_DISPLAY_MODE)) {
-			if (chartData.data != null) {
-				font = new Font("Arial Black", Font.BOLD, 25);
-				graphics.setColor(Color.CYAN);
-				int bottomY = 300;
-				graphics.fillRect(100, bottomY - 100, 40, 100);
-				graphics.fillRect(140, bottomY - 200, 40, 200);
-				graphics.fillRect(180, bottomY - 150, 40, 150);
-				graphics.fillRect(220, bottomY - 125, 40, 125);
-				graphics.fillRect(260, bottomY - 170, 40, 170);
-				graphics.setColor(Color.RED);
-				graphics.setFont(font);
-				graphics.drawString(chartData.data[0], 130, 250);
-				graphics.drawString(chartData.data[1], 130, 270);
-			}
-		} else {
-			int bottomY = 500;
-			graphics.setColor(Color.CYAN);
-			graphics.fillRect(112, bottomY - 200, 75, 200);
-			graphics.fillRect(187, bottomY - 400, 75, 400);
-			graphics.fillRect(262, bottomY - 300, 75, 300);
-			graphics.fillRect(337, bottomY - 250, 75, 250);
-			graphics.fillRect(412, bottomY - 340, 75, 340);
-			font = new Font("Arial Black", Font.BOLD, 55);
-			graphics.setColor(Color.BLACK);
-			graphics.setFont(font);
-			graphics.drawString(chartData.data[0], 130, 400);
 		}
 	}
 
