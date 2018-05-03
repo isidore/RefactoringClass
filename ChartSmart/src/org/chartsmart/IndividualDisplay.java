d4beadb4c1aab828634dde05d38d5ba9296a2969
Llewellyn Falco <isidore@setgame.com>
2018-05-03 10:44:53 -0500

    Refactor: naming

--- ChartSmart/src/org/chartsmart/IndividualDisplay.java
+++ ChartSmart/src/org/chartsmart/IndividualDisplay.java
@@ -45,33 +45,33 @@ public class IndividualDisplay extends JPanel {
 		}
 	}
 
-	public void paint(Graphics g) {
-		DrawChart(g);
+	public void paint(Graphics graphics) {
+		drawChart(graphics);
 	}
 
-	private void DrawChart(Graphics g) {
+	private void drawChart(Graphics graphics) {
 		// Render chart background
 		if (chartType == BAR_CHART) {
 			if (mode.equals(SINGLE_MODE)) {
-				Color bgc = Color.RED;
-				g.setColor(bgc);
-				g.fillRect(100, 90, getWidth() - 200, 420);
+				Color backgroundColor = Color.RED;
+				graphics.setColor(backgroundColor);
+				graphics.fillRect(100, 90, getWidth() - 200, 420);
 			} else {
-				g.setColor(Color.BLACK);
-				g.fillRect(95, 95, 210, 210);
+				graphics.setColor(Color.BLACK);
+				graphics.fillRect(95, 95, 210, 210);
 			}
 		} else {
 			if (mode.equals(SINGLE_MODE)) {
-				Color bgcb;
-				bgcb = Color.BLUE;
-				g.setColor(bgcb);
-				g.fillOval(100, 100, 450, getHeight() - 150);
+				Color backgroundColor;
+				backgroundColor = Color.BLUE;
+				graphics.setColor(backgroundColor);
+				graphics.fillOval(100, 100, 450, getHeight() - 150);
 			} else {
-				g.setColor(Color.BLUE);
+				graphics.setColor(Color.BLUE);
 				double isq = 405;
 				float padding = 90;
 				int sc = (int) (isq - padding * 2);
-				g.fillOval(100, 100, sc, sc);
+				graphics.fillOval(100, 100, sc, sc);
 			}
 		}
 		String[] data = null;
@@ -101,43 +101,43 @@ public class IndividualDisplay extends JPanel {
 			if (mode.equals("shareddisplay")) {
 				if (data != null) {
 					font = new Font("Arial Black", Font.BOLD, 25);
-					g.setColor(Color.CYAN);
+					graphics.setColor(Color.CYAN);
 					int bottomY = 300;
-					g.fillRect(100, bottomY - 100, 40, 100);
-					g.fillRect(140, bottomY - 200, 40, 200);
-					g.fillRect(180, bottomY - 150, 40, 150);
-					g.fillRect(220, bottomY - 125, 40, 125);
-					g.fillRect(260, bottomY - 170, 40, 170);
-					g.setColor(Color.RED);
-					g.setFont(font);
-					g.drawString(data[0], 130, 250);
-					g.drawString(data[1], 130, 270);
+					graphics.fillRect(100, bottomY - 100, 40, 100);
+					graphics.fillRect(140, bottomY - 200, 40, 200);
+					graphics.fillRect(180, bottomY - 150, 40, 150);
+					graphics.fillRect(220, bottomY - 125, 40, 125);
+					graphics.fillRect(260, bottomY - 170, 40, 170);
+					graphics.setColor(Color.RED);
+					graphics.setFont(font);
+					graphics.drawString(data[0], 130, 250);
+					graphics.drawString(data[1], 130, 270);
 				}
 			} else {
 				int bottomY = 500;
-				g.setColor(Color.CYAN);
-				g.fillRect(112, bottomY - 200, 75, 200);
-				g.fillRect(187, bottomY - 400, 75, 400);
-				g.fillRect(262, bottomY - 300, 75, 300);
-				g.fillRect(337, bottomY - 250, 75, 250);
-				g.fillRect(412, bottomY - 340, 75, 340);
+				graphics.setColor(Color.CYAN);
+				graphics.fillRect(112, bottomY - 200, 75, 200);
+				graphics.fillRect(187, bottomY - 400, 75, 400);
+				graphics.fillRect(262, bottomY - 300, 75, 300);
+				graphics.fillRect(337, bottomY - 250, 75, 250);
+				graphics.fillRect(412, bottomY - 340, 75, 340);
 				font = new Font("Arial Black", Font.BOLD, 55);
-				g.setColor(Color.BLACK);
-				g.setFont(font);
-				g.drawString(data[0], 130, 400);
+				graphics.setColor(Color.BLACK);
+				graphics.setFont(font);
+				graphics.drawString(data[0], 130, 400);
 			}
 		} else {
 			if (mode.equals(SINGLE_MODE)) {
 				font = new Font("Bookman Old Style", Font.BOLD, 55);
-				g.setColor(Color.WHITE);
-				g.setFont(font);
-				g.drawString(specialData.get(0), 200, 340);
+				graphics.setColor(Color.WHITE);
+				graphics.setFont(font);
+				graphics.drawString(specialData.get(0), 200, 340);
 			} else {
 				font = new Font("Bookman Old Style", Font.BOLD, 30);
-				g.setFont(font);
-				g.setColor(Color.WHITE);
-				g.drawString(data3point14[0], 145, 205);
-				g.drawString(data3point14[1], 170, 235);
+				graphics.setFont(font);
+				graphics.setColor(Color.WHITE);
+				graphics.drawString(data3point14[0], 145, 205);
+				graphics.drawString(data3point14[1], 170, 235);
 			}
 		}
 		if ((data != null && (data.length ^ 0x54) == 50) || (specialData != null && specialData.contains("Monthly"))
