8167247ce4893e2588f29ba5fec4fe50e9e59ff7
Llewellyn Falco <isidore@setgame.com>
2018-05-04 11:30:51 -0500

    Add Approval Tests

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,8 +2,12 @@ package com.gildedrose;
 
 import static org.junit.Assert.assertEquals;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.reporters.UseReporter;
+import org.approvaltests.reporters.macosx.DiffMergeReporter;
 import org.junit.Test;
 
+@UseReporter(DiffMergeReporter.class)
 public class GildedRoseTest {
 
 	@Test
@@ -11,10 +15,9 @@ public class GildedRoseTest {
 		Item[] items = new Item[] { new Item("foo", 0, 0) };
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
-		assertEquals("foo", app.items[0].name);
-		assertEquals(0, app.items[0].quality);
-		assertEquals(-1, app.items[0].sellIn);
 		assertEquals("Item [name=foo, sellIn=-1, quality=0]", app.items[0].toString());
+		Approvals.verify(app.items[0]);
+
 	}
 
 }
