eb390a03ff4bc0e44a736c640143f41658555bf3
Llewellyn Falco <isidore@setgame.com>
2018-05-04 16:15:54 -0500

    woody's refactoring

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,9 +1,6 @@
 package com.gildedrose;
 
 class GildedRose {
-	private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
-	private static final String AGED_BRIE = "Aged Brie";
-	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
 	Item[] items;
 
 	public GildedRose(Item[] items) {
@@ -11,43 +8,80 @@ class GildedRose {
 	}
 
 	public void updateQuality() {
-		for (Item item : items) {
-			AgeItem ageItem = getAgeItem(item);
-			ageItem.age(item);
+		for (int i = 0; i < items.length; i++) {
 
-			decrementSellInUnlessSulfuras(item);
-
-			if (item.sellIn < 0) {
-				ageItem.expire(item);
+			Item item = items[i];
+			if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+				continue;
 			}
+			if (item.name.equals("Aged Brie")) {
+				doAgedBrie(item);
+				continue;
+			}
+			if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+				doBackstagePass(item);
+				continue;
+			}
+			doNormal(item);
+
 		}
 	}
 
-	public void decrementSellInUnlessSulfuras(Item item) {
-		if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-			return;
+	public void doNormal(Item item) {
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
+
+		}
+
+		item.sellIn = item.sellIn - 1;
+
+		if (item.sellIn < 0) {
+
+			if (item.quality > 0) {
+				item.quality = item.quality - 1;
+
+			}
+
+		}
+	}
+
+	public void doBackstagePass(Item item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
+
+			if (item.sellIn < 11) {
+				if (item.quality < 50) {
+					item.quality++;
+				}
+			}
+
+			if (item.sellIn < 6) {
+				if (item.quality < 50) {
+					item.quality++;
+				}
+			}
+
 		}
 		item.sellIn--;
+
+		if (item.sellIn < 0) {
+
+			item.quality = 0;
+		}
 	}
 
-	public AgeItem getAgeItem(Item item) {
-		AgeItem ageItem = null;
-		if (item.name.equals(AGED_BRIE)) {
-			ageItem = new Brie();
-		} else if (item.name.equals(BACKSTAGE_PASSES)) {
-			ageItem = new BackstagePass();
-		} else if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-			ageItem = new Sulfuras();
-		} else {
-			ageItem = new DefaultAgeItem();
-		}
-		return ageItem;
-	}
+	public void doAgedBrie(Item item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
 
-	public static void incrementQualityUnlessGreaterThan50(Item item) {
-		if (item.quality >= 50) {
-			return;
 		}
-		item.quality++;
+
+		item.sellIn = item.sellIn - 1;
+
+		if (item.sellIn < 0) {
+			if (item.quality < 50) {
+				item.quality = item.quality + 1;
+			}
+		}
 	}
 }
