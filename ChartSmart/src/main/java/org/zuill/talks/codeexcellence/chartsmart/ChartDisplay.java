ee05d44fbdc401fc926ef10a452d31aa7c3218e1
Llewellyn Falco <isidore@setgame.com>
2018-07-17 14:33:49 -0600

    r rename blue

--- ChartSmart/src/main/java/org/zuill/talks/codeexcellence/chartsmart/ChartDisplay.java
+++ ChartSmart/src/main/java/org/zuill/talks/codeexcellence/chartsmart/ChartDisplay.java
@@ -76,8 +76,7 @@ public class ChartDisplay extends JPanel {
 			}
 		} else {
 			if (dimension.equals(SINGLE_MODE_CHART)) {
-				Color blue;
-				blue = Color.BLUE;
+				Color blue = Color.BLUE;
 				graphics.setColor(blue);
 				graphics.fillOval(100, 100, 450, getHeight() - 150);
 			} else {
