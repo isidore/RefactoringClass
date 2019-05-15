cca73f4e5b22844c4f2c985fa32242000ec481f2
Llewellyn Falco <isidore@setgame.com>
2019-05-15 15:30:26 +0200

    !!! renamed iniDS to showChart manually

--- ChartSmart/ChartSmart/src/org/zuill/talks/codeexcellence/chartsmart/ChartData.java
+++ ChartSmart/ChartSmart/src/org/zuill/talks/codeexcellence/chartsmart/ChartData.java
@@ -1,5 +1,11 @@
 package org.zuill.talks.codeexcellence.chartsmart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChartData
 {
+  public String[]     data        = null;
+  public List<String> specialData = new ArrayList<String>();
+  public String[]     pieData     = new String[0];
 }
