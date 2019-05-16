4719735675a66008f99ae6bf6e15eda1663ab81a
Llewellyn Falco <isidore@setgame.com>
2019-05-16 12:02:51 +0300

    Moved to maven.

--- ChartSmart/ChartSmart.Java/src/test/java/org/zuill/talks/codeexcellence/chartsmart/ChartSmartTest.java
+++ ChartSmart/ChartSmart.Java/src/test/java/org/zuill/talks/codeexcellence/chartsmart/ChartSmartTest.java
@@ -1,21 +1,22 @@
-package org.zuill.talks.codeexcellence.chartsmart.tests;
+package org.zuill.talks.codeexcellence.chartsmart;
+
+import static org.junit.Assert.assertEquals;
 
 import org.approvaltests.Approvals;
 import org.approvaltests.reporters.DelayedClipboardReporter;
 import org.approvaltests.reporters.UseReporter;
-import org.zuill.talks.codeexcellence.chartsmart.ChartSmart;
-import org.zuill.talks.codeexcellence.chartsmart.ChartWindow;
-
-import junit.framework.TestCase;
+import org.junit.Test;
 
 @UseReporter(DelayedClipboardReporter.class)
-public class ChartSmartTest extends TestCase
+public class ChartSmartTest
 {
+  @Test
   public void testMainWindow() throws Exception
   {
     ChartSmart chartSmart = new ChartSmart();
     Approvals.verify(chartSmart);
   }
+  @Test
   public void testBarChartWindow() throws Exception
   {
     ChartWindow chartSmart = new ChartWindow();
@@ -23,6 +24,7 @@ public class ChartSmartTest extends TestCase
     Approvals.verify(chartSmart);
     assertEquals("Bar Chart - Single Mode", chartSmart.getTitle());
   }
+  @Test
   public void testPieChartWindow() throws Exception
   {
     ChartWindow chartSmart = new ChartWindow();
@@ -30,6 +32,7 @@ public class ChartSmartTest extends TestCase
     Approvals.verify(chartSmart);
     assertEquals("Pie Chart - Single Mode", chartSmart.getTitle());
   }
+  @Test
   public void testBarChartSmallWindow() throws Exception
   {
     ChartWindow chartSmart = new ChartWindow();
@@ -37,6 +40,7 @@ public class ChartSmartTest extends TestCase
     Approvals.verify(chartSmart);
     assertEquals("Bar Chart - Compare Mode", chartSmart.getTitle());
   }
+  @Test
   public void testPieChartSmallWindow() throws Exception
   {
     ChartWindow chartSmart = new ChartWindow();
