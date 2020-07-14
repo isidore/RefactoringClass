4c03ac7ef66b03d60e8ae551861993b0e46450e1
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-07-14 22:18:59 +0000

    r   extract method
    Co-Authored-By: mjmccormac <mjmccormac@users.noreply.github.com>
    Co-Authored-By: Zak <vashzak@users.noreply.github.com>
    Co-Authored-By: Clare Macrae <github@cfmacrae.fastmail.co.uk>
    Co-Authored-By: Ron Jennings <rhjennings@users.noreply.github.com>
    Co-Authored-By: joelkuenning <joelkuenning@users.noreply.github.com>
    Co-Authored-By: goblinsan <goblinsan@users.noreply.github.com>

--- ChartSmart/ChartSmart.Java/src/main/java/org/zuill/talks/codeexcellence/chartsmart/ChartWindow.java
+++ ChartSmart/ChartSmart.Java/src/main/java/org/zuill/talks/codeexcellence/chartsmart/ChartWindow.java
@@ -111,38 +111,7 @@ public class ChartWindow extends JPanel
     Font font;
     if (chartId == BAR_CHART_ID)
     {
-      if (chartMode.equals(Modes.SHARED_DISPLAY))
-      {
-        if (data != null)
-        {
-          font = new Font("Arial Black", Font.BOLD, 25);
-          graphics.setColor(Color.CYAN);
-          int bottomY = 300;
-          graphics.fillRect(100, bottomY - 100, 40, 100);
-          graphics.fillRect(140, bottomY - 200, 40, 200);
-          graphics.fillRect(180, bottomY - 150, 40, 150);
-          graphics.fillRect(220, bottomY - 125, 40, 125);
-          graphics.fillRect(260, bottomY - 170, 40, 170);
-          graphics.setColor(Color.RED);
-          graphics.setFont(font);
-          graphics.drawString(data[0], 130, 250);
-          graphics.drawString(data[1], 130, 270);
-        }
-      }
-      else
-      {
-        int bottomY = 500;
-        graphics.setColor(Color.CYAN);
-        graphics.fillRect(112, bottomY - 200, 75, 200);
-        graphics.fillRect(187, bottomY - 400, 75, 400);
-        graphics.fillRect(262, bottomY - 300, 75, 300);
-        graphics.fillRect(337, bottomY - 250, 75, 250);
-        graphics.fillRect(412, bottomY - 340, 75, 340);
-        font = new Font("Arial Black", Font.BOLD, 55);
-        graphics.setColor(Color.BLACK);
-        graphics.setFont(font);
-        graphics.drawString(data[0], 130, 400);
-      }
+      adventures(graphics, data);
     }
     else
     {
@@ -161,6 +130,42 @@ public class ChartWindow extends JPanel
         graphics.drawString(data3point14[0], 145, 205);
         graphics.drawString(data3point14[1], 170, 235);
       }
+    }
+  }
+
+  private void adventures(Graphics graphics, String[] data) {
+    Font font;
+    if (chartMode.equals(Modes.SHARED_DISPLAY))
+    {
+      if (data != null)
+      {
+        font = new Font("Arial Black", Font.BOLD, 25);
+        graphics.setColor(Color.CYAN);
+        int bottomY = 300;
+        graphics.fillRect(100, bottomY - 100, 40, 100);
+        graphics.fillRect(140, bottomY - 200, 40, 200);
+        graphics.fillRect(180, bottomY - 150, 40, 150);
+        graphics.fillRect(220, bottomY - 125, 40, 125);
+        graphics.fillRect(260, bottomY - 170, 40, 170);
+        graphics.setColor(Color.RED);
+        graphics.setFont(font);
+        graphics.drawString(data[0], 130, 250);
+        graphics.drawString(data[1], 130, 270);
+      }
+    }
+    else
+    {
+      int bottomY = 500;
+      graphics.setColor(Color.CYAN);
+      graphics.fillRect(112, bottomY - 200, 75, 200);
+      graphics.fillRect(187, bottomY - 400, 75, 400);
+      graphics.fillRect(262, bottomY - 300, 75, 300);
+      graphics.fillRect(337, bottomY - 250, 75, 250);
+      graphics.fillRect(412, bottomY - 340, 75, 340);
+      font = new Font("Arial Black", Font.BOLD, 55);
+      graphics.setColor(Color.BLACK);
+      graphics.setFont(font);
+      graphics.drawString(data[0], 130, 400);
     }
   }
 
