a6c21749ac6687984c07855fb47fb581d47b8593
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-07-14 18:49:40 +0000

    r   add nonsense name
    Co-Authored-By: mjmccormac <mjmccormac@users.noreply.github.com>
    Co-Authored-By: Zak <vashzak@users.noreply.github.com>
    Co-Authored-By: Clare Macrae <github@cfmacrae.fastmail.co.uk>
    Co-Authored-By: Ron Jennings <rhjennings@users.noreply.github.com>
    Co-Authored-By: joelkuenning <joelkuenning@users.noreply.github.com>
    Co-Authored-By: goblinsan <goblinsan@users.noreply.github.com>

--- ChartSmart/ChartSmart.Java/src/main/java/org/zuill/talks/codeexcellence/chartsmart/ChartWindow.java
+++ ChartSmart/ChartSmart.Java/src/main/java/org/zuill/talks/codeexcellence/chartsmart/ChartWindow.java
@@ -13,6 +13,7 @@ import javax.swing.JPanel;
 
 public class ChartWindow extends JPanel
 {
+  public static final int STUFF = 406;
   private String   jjD;
   private String   __APARAM__Z;
 
@@ -21,7 +22,7 @@ public class ChartWindow extends JPanel
   private void initializeDrawArea()
   {
     this.setPreferredSize(new Dimension(600, 600));
-    if (ct == 406)
+    if (ct == STUFF)
     {
       if (jjD.equals("rpfll"))
       {
@@ -79,7 +80,7 @@ public class ChartWindow extends JPanel
   private void DrawChart(Graphics g)
   {
     // Render chart background
-    if (ct == 406)
+    if (ct == STUFF)
     {
       if (jjD.equals("rpfll"))
       {
@@ -114,7 +115,7 @@ public class ChartWindow extends JPanel
     String[] data = null;
     List<String> specialData = new ArrayList<String>();
     String[] data3point14 = new String[0];
-    if (ct == 406)
+    if (ct == STUFF)
     {
       if (jjD.equals("rpfll"))
       {
@@ -143,7 +144,7 @@ public class ChartWindow extends JPanel
       }
     }
     Font font;
-    if (ct == 406)
+    if (ct == STUFF)
     {
       if (jjD.equals("shareddisplay"))
       {
